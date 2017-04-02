b57897efc96e99a6622b8d442deccd257d01e289
v1rt <ronneilcamara@gmail.com>
2017-04-01 23:52:17 -0500

    Initial commit ...

---
+++ index.js
@@ -0,0 +1,9 @@
+const ServerNames = {
+  "webServer": "http://www.example.com",
+  "toolsServer": "http://tools.example.com",
+  "medicontentServer": "http://medicontent.example.com",
+  "apiServer": "https://api.example.com",
+  "custhelpServer": "https://custhelp.example.com"
+}
+
+export { ServerNames }
