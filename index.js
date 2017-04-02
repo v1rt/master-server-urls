25c12588f9b3e7916fbb11b991634177235ff40a
v1rt <ronneilcamara@gmail.com>
2017-04-02 00:41:39 -0500

    Changed to module.exports ...

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const ServerNames = {
-  "webServer": "http://www.example.com",
-  "toolsServer": "http://tools.example.com",
-  "medicontentServer": "http://medicontent.example.com",
-  "apiServer": "https://api.example.com",
-  "custhelpServer": "https://custhelp.example.com"
+module.exports = {
+  "ServerNames": {
+    "webServer": "http://www.example.com",
+    "toolsServer": "http://tools.example.com",
+    "medicontentServer": "http://medicontent.example.com",
+    "apiServer": "https://api.example.com",
+    "custhelpServer": "https://custhelp.example.com"
+  }
 }
-
-export { ServerNames }
